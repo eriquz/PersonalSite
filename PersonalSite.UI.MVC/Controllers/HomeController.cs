401ba71cbdb9d980fde8ac8407cf6cab256acde8
Enrique Garcia <EriquzGar10@outlook.com>
2022-04-14 05:24:49 -0500

    Added template and Conversion

---
+++ PersonalSite.UI.MVC/Controllers/HomeController.cs
@@ -0,0 +1,48 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace PersonalSite.UI.MVC.Controllers
+{
+    public class HomeController : Controller
+    {
+        public ActionResult Index()//Home
+        {
+            return View();
+        }
+
+        public ActionResult About()
+        {
+            return View();
+        }
+
+        public ActionResult Contact()
+        {
+            ViewBag.Message = "Your contact page.";
+
+            return View();
+        }
+
+        public ActionResult Links()
+        {
+            
+
+            return View();
+        }
+
+        public ActionResult Resume()
+        {
+            
+
+            return View();
+        }
+        public ActionResult Portfolio()
+        {
+            
+
+            return View();
+        }
+    }
+}
